29594cbd4e9ead036625444edfdfbfaeac011f7e
manasa <mmanasa722@gmail.com>
2025-03-12 00:00:10 +0530

    ComposeQuadrant_App

--- app/src/main/java/com/example/articlepage_compose_app/MainActivity.kt
+++ app/src/main/java/com/example/articlepage_compose_app/MainActivity.kt
@@ -5,16 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,9 +35,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArticlePage_Compose_AppTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    Greeting(
+                    /*Greeting(
                         name = "Android"
-                    )
+                    )*/
+                    ComposeQuadrant()
                 }
             }
         }
@@ -62,10 +70,63 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun ComposeQuadrant(){
+        Column(Modifier.fillMaxWidth()) {
+            Row(Modifier.weight(1f)) {
+                QuadrantItem(
+                    heading = "Text composable",
+                    description = "Displays text and follows the recommended Material Design guidelines.",
+                    backgroundColor = Color(0xFFEADDFF),
+                    modifier = Modifier.weight(1f))
+                QuadrantItem(
+                    heading = "Image composable",
+                    description = "Creates a composable that lays out and draws a given Painter class object.",
+                    backgroundColor = Color(0xFFD0BCFF),
+                    modifier = Modifier.weight(1f))
+            }
+            Row(modifier = Modifier.weight(1f)) {
+                QuadrantItem(
+                    heading = "Row composable",
+                    description = "A layout composable that places its children in a horizontal sequence.",
+                    backgroundColor = Color(0xFFB69DF8),
+                    modifier = Modifier.weight(1f))
+                QuadrantItem(
+                    heading = "Column composable",
+                    description = "A layout composable that places its children in a vertical sequence.",
+                    backgroundColor = Color(0xFFF6EDFF),
+                    modifier = Modifier.weight(1f))
+            }
+        }
+}
+
+@Composable
+fun QuadrantItem(heading: String,description: String, backgroundColor: Color, modifier: Modifier = Modifier) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(backgroundColor)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = heading,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = description,
+                textAlign = TextAlign.Justify
+            )
+        }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArticlePage_Compose_AppTheme {
-        Greeting("Android")
+        /*Greeting("Android")*/
+        ComposeQuadrant()
     }
 }
